2b0bf4163116821ff7646c3ca947421a5c87e800
Elham-EN <ilhamrezaie@gmail.com>
2022-01-20 21:22:48 +1100

    Populate planets data before listening for request using asycn function

--- src/routes/planets/planets.controller.js
+++ src/routes/planets/planets.controller.js
@@ -3,7 +3,7 @@
  * work with them to update the model
  */
 
-const planets = require("../../models/planets.model");
+const { planets } = require("../../models/planets.model");
 
 function getAllPlanets(req, res) {
   return res.status(200).json(planets);
--- src/server.js
+++ src/server.js
@@ -4,11 +4,23 @@ const http = require("http");
 const PORT = process.env.PORT || 8000;
 const app = require("./app"); //express app is a listener
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 //Take request listener function as argument, which responds to all
 //incoming request to our server. But what handle our requests? what do
 //we pass in the createServer?
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+//Use this function when we need to load some data or perform some actions
+//before our server actually starts responding to the user
+async function startServer() {
+  //To make sure the planets data is available for any request that ever
+  //comes into our server. Populate planets data before listening for request
+  await loadPlanetsData();
+  //Listening on Port 8000. Listening for a request from the client
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
+
+startServer();
